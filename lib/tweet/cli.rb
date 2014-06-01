f15f90319171a2da076543d94081b37a42a45b9f
betahikaru <beta.hikaru2000+bitbucket@gmail.com>
2014-06-02 01:14:16 +0900

    Remove Tweet::Cli::Application.post. Because it conflict T::CLI.update.

--- lib/tweet/cli.rb
+++ lib/tweet/cli.rb
@@ -5,10 +5,6 @@ require "t/cli"
 module Tweet
   module Cli
     class Application < T::CLI
-      desc "post WORD", "post Hello,Twitter"
-      def post(word)
-        say(word)
-      end
     end
   end
 end
